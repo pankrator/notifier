dd85d06a4f571177cf9a6601251da876420bab82
Nikolay Stoykov <nikolay.stoykov@theopentag.com>
2023-07-04 23:40:06 +0300

    Describe notification metadata.

--- internal/entity/notification.go
+++ internal/entity/notification.go
@@ -26,6 +26,10 @@ type Notification struct {
 	Type      NotificationType `db:"type"`
 	Message   string           `db:"message"`
 	Recipient string           `db:"recipient"`
-	Metadata  json.RawMessage  `db:"metadata"`
-	CreatedAt time.Time        `db:"created_at"`
+	// Metadata for other types of notifications more data might be needed that is not generic to all types
+	// it can be saved in metadata.
+	// Other approach would be to have join tables with additional data for certain types of notifications
+	// that need the additional data.
+	Metadata  json.RawMessage `db:"metadata"`
+	CreatedAt time.Time       `db:"created_at"`
 }
